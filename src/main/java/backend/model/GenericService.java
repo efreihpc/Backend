e221250d282b9d9baccec64f0b8ad5beec93e1ed
Stix <stix@phcn.de>
2015-02-05 21:03:29 +0100

    roughly finished frame

--- src/main/java/backend/model/GenericService.java
+++ src/main/java/backend/model/GenericService.java
@@ -1,5 +1,23 @@
 package backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@Entity
+@Inheritance
+@JsonTypeInfo(  
+	    use = JsonTypeInfo.Id.NAME,  
+	    include = JsonTypeInfo.As.PROPERTY,  
+	    property = "type")  
 public abstract class GenericService<T> implements Service<T> {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private long m_id;
+    private String name;
 }
--- src/main/java/backend/model/GenericServiceProvider.java
+++ src/main/java/backend/model/GenericServiceProvider.java
@@ -1,5 +1,23 @@
 package backend.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@Entity
+@Inheritance
+@JsonTypeInfo(  
+	    use = JsonTypeInfo.Id.NAME,  
+	    include = JsonTypeInfo.As.PROPERTY,  
+	    property = "type")  
 public abstract class GenericServiceProvider implements ServiceProvider{
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+	private long m_id;
+    private String name;
 }
