d51f351dd6bbd21e2f719ba3b80b3dd71bc4e90c
Stix <stix@phcn.de>
2015-03-06 17:22:11 +0100

    added quandl data job

---
+++ plugins/SPHPC/src/main/java/backend/model/SPHPC/QuandlDataRetrievalJob.java
@@ -0,0 +1,14 @@
+package backend.model.SPHPC;
+
+import backend.model.job.JobEntity;
+import backend.model.result.JsonResult;
+
+public class QuandlDataRetrievalJob extends JobEntity<JsonResult> {
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
