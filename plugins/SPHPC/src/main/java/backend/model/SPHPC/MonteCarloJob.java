35d1484805531cc8cb85d2abd541ab2329e113df
Stix <stix@phcn.de>
2015-03-14 23:36:43 +0100

    added montecarlo job

---
+++ plugins/SPHPC/src/main/java/backend/model/SPHPC/MonteCarloJob.java
@@ -0,0 +1,14 @@
+package backend.model.SPHPC;
+
+import backend.model.job.JobPlugin;
+import backend.model.result.JsonResult;
+
+public class MonteCarloJob extends JobPlugin<JsonResult> {
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
