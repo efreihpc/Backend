f6b4f72460f11129741507ac1be36c09c9a04c95
Stix <stix@phcn.de>
2015-03-29 22:40:56 +0200

    implemented basic montecarloservice frontend

--- application/static/js/Job.js
+++ application/static/js/Job.js
@@ -1,38 +1,77 @@
 $(document).ready(function(){
-	refreshJobIndex();
-	$("#Index").submit(function() { return false; });
-	$("#Index #Refresh").click(function(){
-		refreshJobIndex();
-	});
-	$("#Index #Check").click(function(){
-		addCheckStatusJob();
-		refreshJobIndex();
-	});
+	$("#MonteCarlo").submit(function() { return false; });
+	$("#MonteCarlo #Query").click(function(){monteCarloQuery($("#MonteCarlo #SearchField").val());});
 });
-
-function refreshJobIndex()
-{
-	getAllJobs();
-}
-
-function getAllJobs()
-{
-  $.getJSON(
-    "http://localhost:8080/job/",
-    function( data ) {
-      var items = [];
-      $.each( data, function( key, val ) {
-        items.push( "<li class=\"media\"><a class=\"media-left\" href=\"/job.html?identifier=" + val.id + "\"><span class=\"glyphicon glyphicon-tag\" aria-hidden=\"true\"></a><div class=\"media-body\"></span><h4 class=\"media-heading\">ID: " + val.id + "</h4><p>Name: " + val.name + "</p></div></li>" );
-      });
-      $( "#JobIndex" ).replaceWith($( "<ul/>", {
-        "class": "media-list",
-        "id": "JobIndex",
-        html: items.join( "" )
-      }));
-    });
-}
 
 function addCheckStatusJob()
 {
-	  $.getJSON("http://localhost:8080/state");
+  $.getJSON("http://localhost:8080/state");
+}
+
+function displayService(jsonData)
+{
+  $.each(jsonData, function(key, val)
+  {
+      $("<p>" + val.id + "</p>").appendTo("#Services");
+  });
+}
+
+function monteCarloQuery(searchTerm)
+{
+  configuration = {
+    stockId: searchTerm,
+    OauthToken: ""
+  }
+
+  queryService("ba6527deaac9505f6db41b10c6424ee463f4c2df", configuration, displayService);
+}
+
+function queryService(ServiceIdentifier, configuration, onFinish)
+{
+  serviceTransfer = {
+    descriptor: {
+      identifier: ServiceIdentifier
+    },
+    configuration: configuration
+  }
+
+  doWithService(serviceTransfer, pollService, onFinish);
+}
+
+function pollService(serviceUid, onFinish)
+{
+  setInterval(function() {doOnServiceFinishJsonId(serviceUid, onFinish);}, 5000);
+}
+
+function doOnServiceFinishJsonId(serviceUid, finishedFunction)
+{
+  $.getJSON(
+    "http://localhost:8080/service/id/" + serviceUid,
+    function(data){ doOnServiceFinishJson(data, finishedFunction);});
+}
+
+function doOnServiceFinishJson(jsonData, finishedFunction)
+{
+  $.each(jsonData, function(key, val)
+  {
+    if(val.state == 1.0)
+      finishedFunction(jsonData);
+  });
+}
+
+function doWithService(serviceTransfer, subject, object)
+{
+  jQuery.ajax ({
+    url: "http://localhost:8080/backend/schedule/",
+    type: "POST",
+    data: JSON.stringify(serviceTransfer),
+    dataType: "json",
+    contentType: "application/json; charset=utf-8",
+    success: function(responsedata) {doWithServiceJson(responsedata, subject, object);}
+  });
+}
+
+function doWithServiceJson(jsonData, subject, object)
+{
+    subject(jsonData.id, object);
 }
